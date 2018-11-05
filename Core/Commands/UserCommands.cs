6049f229239d06fe5b992580ec990fc5d42756d2
ValgeHunt <kaspar098@hotmail.com>
2018-11-05 12:23:05 +0200

    Add files via upload

---
+++ Core/Commands/UserCommands.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.Text;
+using System.Linq;
+using Discord.WebSocket;
+using System.Globalization;
+
+using Discord;
+using Discord.Commands;
+
+namespace Valge_bot.Core.Commands
+{
+    public class HelloWorld : ModuleBase<SocketCommandContext>
+    {
+        [Command("hello")]
+        public async Task ValgeHunt()
+        {
+            await Context.Channel.SendMessageAsync("Hello World!");
+        }
+    }
+
+    public class GiveDj : ModuleBase<SocketCommandContext>
+    {
+        [Command("dj")]
+        public async Task GiveDj2()
+        {
+            var user = (IGuildUser)Context.User;
+            var role = Context.Guild.Roles.FirstOrDefault(x => x.Name.ToString() == "DJ");
+            await user.AddRoleAsync(role);
+            await Context.Channel.SendMessageAsync("DJ role given!");
+        }
+
+    }
+
+    public class GiveWinRanks : ModuleBase<SocketCommandContext>
+    {
+        [Command("wins")]
+        public async Task GiveWinRanks2([Remainder]string input ="0")
+        {
+            int userEnteredNumber = 0;
+            bool ifIsNumber = int.TryParse(input, out userEnteredNumber);
+            if (ifIsNumber)
+            {
+                var user = (IGuildUser)Context.User;
+                CultureInfo ci = new CultureInfo("en-US");
+                IEnumerable<IRole> roles = (IEnumerable < IRole > )Context.Guild.Roles.Where(x => x.Name.ToString().StartsWith("Tier", true, ci));
+                await user.RemoveRolesAsync(roles);
+
+                if (userEnteredNumber <= 0)
+                {
+                    var role = Context.Guild.Roles.FirstOrDefault(x => x.Name.ToString() == "Tier 0 (New)");
+                    await user.AddRoleAsync(role);
+                    await Context.Channel.SendMessageAsync("Tier 0 given");
+                }
+
+                if (userEnteredNumber >= 1 && userEnteredNumber <= 25)
+                {
+                    var role = Context.Guild.Roles.FirstOrDefault(x => x.Name.ToString() == "Tier 1 (Beginner)");
+                    await user.AddRoleAsync(role);
+                    await Context.Channel.SendMessageAsync("Tier 1 given");
+                }
+
+                if (userEnteredNumber >= 26 && userEnteredNumber <= 75)
+                {
+                    var role = Context.Guild.Roles.FirstOrDefault(x => x.Name.ToString() == "Tier 2 (Intermediate)");
+                    await user.AddRoleAsync(role);
+                    await Context.Channel.SendMessageAsync("Tier 2 given");
+                }
+
+                if (userEnteredNumber >= 76 && userEnteredNumber <= 150)
+                {
+                    var role = Context.Guild.Roles.FirstOrDefault(x => x.Name.ToString() == "Tier 3 (Proficient)");
+                    await user.AddRoleAsync(role);
+                    await Context.Channel.SendMessageAsync("Tier 3 given");
+                }
+
+                if (userEnteredNumber >= 151 && userEnteredNumber <= 300)
+                {
+                    var role = Context.Guild.Roles.FirstOrDefault(x => x.Name.ToString() == "Tier 4 (Advanced)");
+                    await user.AddRoleAsync(role);
+                    await Context.Channel.SendMessageAsync("Tier 4 given");
+                }
+
+                if (userEnteredNumber >= 300)
+                {
+                    var role = Context.Guild.Roles.FirstOrDefault(x => x.Name.ToString() == "Tier 5 (Elite)");
+                    await user.AddRoleAsync(role);
+                    await Context.Channel.SendMessageAsync("Tier 5 given");
+                }
+
+            }
+
+            else
+            {
+                await Context.Channel.SendMessageAsync("Insert a valid number!");
+            }
+              
+        }
+
+    }
+
+}
---
+++ Program.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.IO;
+using System.Reflection;
+using System.Threading.Tasks;
+
+using Discord;
+using Discord.Commands;
+using Discord.WebSocket;
+
+
+namespace Valge_bot
+{
+    class Program
+    {
+        private DiscordSocketClient Client;
+        private CommandService Commands;
+
+        static void Main(string[] args)
+        => new Program().Mainasync().GetAwaiter().GetResult();
+
+        private async Task Mainasync()
+        {
+            Client = new DiscordSocketClient(new DiscordSocketConfig
+            {
+                LogLevel = LogSeverity.Debug
+            });
+
+            Commands = new CommandService(new CommandServiceConfig
+            {
+                CaseSensitiveCommands = true,
+                DefaultRunMode = RunMode.Async,
+                LogLevel = LogSeverity.Debug
+            });
+
+            Client.MessageReceived += Client_MessageReceived;
+            await Commands.AddModulesAsync(Assembly.GetEntryAssembly());
+
+            Client.Ready += Client_Ready;
+            Client.Log += Client_Log;
+
+            string Token = "";
+            using (var Stream = new FileStream((Path.GetDirectoryName(Assembly.GetEntryAssembly().Location)).Replace(@"bin\Debug\netcoreapp2.1", @"Data\Token.txt"), FileMode.Open, FileAccess.Read))
+            using (var ReadToken = new StreamReader(Stream))
+            {
+                Token = ReadToken.ReadToEnd();
+            }
+            await Client.LoginAsync(TokenType.Bot, Token);
+            await Client.StartAsync();
+
+            await Task.Delay(-1);
+        }
+
+        private async Task Client_Log(LogMessage Message)
+        
+        {
+            Console.WriteLine($"{DateTime.Now} at {Message.Source}] {Message.Message}");
+        }
+
+        private async Task Client_Ready()
+        {
+            await Client.SetGameAsync("TestBot", "", ActivityType.Playing);
+        }
+
+        private async Task Client_MessageReceived(SocketMessage MessageParam)
+        {
+            var Message = MessageParam as SocketUserMessage;
+            var Context = new SocketCommandContext(Client, Message);
+
+            if (Context.Message == null || Context.Message.Content == "")
+            {
+                return;
+            }
+
+            if (Context.User.IsBot)
+            {
+                return;
+            }
+
+            int ArgPos = 0;
+            if (!(Message.HasStringPrefix("*", ref ArgPos)))
+            {
+                return;
+            }
+
+            var Result = await Commands.ExecuteAsync(Context, ArgPos);
+            if (!Result.IsSuccess)
+            {
+                Console.WriteLine($"{DateTime.Now} at Commands] Something went wrong with executing a command. Text: {Context.Message.Content} | Error: {Result.ErrorReason}");
+            }
+        }
+    }
+}
